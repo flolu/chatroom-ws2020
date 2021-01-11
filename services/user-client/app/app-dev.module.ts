af2bba71b8f93f7e5cc692740370d8f76a16ad92
flolu <flo@flolu.com>
2021-01-11 14:40:09 +0100

    feat(user-client): setup redux

--- services/user-client/app/app-dev.module.ts
+++ services/user-client/app/app-dev.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core'
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
+import {RootStoreModule} from '@store'
 import {AppBaseModule} from './app-base.module'
 import {AppComponent} from './app.component'
 
 @NgModule({
-  imports: [AppBaseModule],
+  imports: [AppBaseModule, RootStoreModule, StoreDevtoolsModule.instrument()],
   bootstrap: [AppComponent],
 })
 export class AppDevModule {
